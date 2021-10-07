5c2511e793af27ed95ee74ed75e6e42c9b79902e
zahar_studennikov <zhrblondy@gmail.com>
2021-10-08 02:04:26 +0300

    Spring Boot + Spring Data

---
+++ src/main/java/com/example/demo/dto/PostDTO.java
@@ -0,0 +1,76 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+@Data
+public class PostDTO {
+
+    private Long id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String caption;
+    private String location;
+    private String username;
+    private Integer likes;
+    private Set<String> usersLiked;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Set<String> getUsersLiked() {
+        return usersLiked;
+    }
+
+    public void setUsersLiked(Set<String> usersLiked) {
+        this.usersLiked = usersLiked;
+    }
+}
---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,57 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    @NotEmpty
+    private String firstname;
+    @NotEmpty
+    private String lastname;
+    private String username;
+    private String bio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
